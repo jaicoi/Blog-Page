79432969bca679154fb8baa553a189326faa0fc2
jaicoi <151331562+jaicoi@users.noreply.github.com>
2024-02-05 19:23:47 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Blog Section</title>
+    <link rel="stylesheet" type="text/css" href="blog.css"/>
+    <link rel="shortcut icon" href="images/fav-icon.svg"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <section id="blog">
+            <div class="blog-heading">
+              <span>My Blog Site</span>
+              <h3>Anime</h3>
+            </div>
+      
+      
+            <div class="blog-container">
+      
+              <div class="blog-box">
+      
+
+                <div class="blog-img">
+                  <img alt="blog" src="picture/op.jfif">
+                </div>
+
+                <div class="blog-text">
+                  <span>This is Luffy</span>
+                  <a href="#" class="blog-title">Gear 5</a>
+                  <p>Gear 5 is a ridiculous-yet-formidable transformation spurred into being by the awakening of the Gomu Gomu no Mi, the legendary Zoan-type Devil Fruit that allows its ingester to stretch to epic proportions, effectively turning them into a “rubber human” of sorts. Having already unlocked the powers of Gears 2, 3, and 4, Luffy realizes this latest Gear 5 ability during his final battle with antagonist Kaidou of the Beasts on Onigashima, which takes place in Chapter 1,044 of the One Piece manga.</p>
+                  <a href="faker.html">See More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+   
+              <div class="blog-box">
+      
+                <div class="blog-img">
+                  <img alt="blog" src="picture/nr.jpeg">
+                </div>
+                <div class="blog-text">
+                  <span>My Robin</span>
+                  <a href="#" class="blog-title">Nico Robin</a>
+                  <p>Nico Robin,[17] also known by her epithet "Devil Child"[7] and the "Light of the Revolution",[8] is the archaeologist of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet.[3] She is the seventh member of the crew and the sixth to join, doing so at the end of the Arabasta Arc. She temporarily left the crew during the Water 7 Arc but rejoined during the Enies Lobby Arc.</p>
+                  <a href="faker.html">See More</a>
+                </div>
+      
+              </div>
+      
+      
+      
+      
+              
+              <div class="blog-box">
+      
+                
+                <div class="blog-img">
+                  <img alt="blog" src="picture/am.jfif">
+                </div>
+                
+                <div class="blog-text">
+                  <span>favorite foods</span>
+                  <a href="#" class="blog-title">Adobong Manok</a>
+                  <p>Adobong manok is a traditional stew originating from the Philippines. This type of Filipino adobo is made with a combination of chicken, garlic, onions, bay leaves, peppercorns, oil, brown sugar, soy sauce, and white vinegar. The chicken is cut into pieces and marinated in a mixture of all the mentioned ingredients (except oil).</p>
+                  <a href="faker.html">Read More</a>
+                </div>
+      
+              </div>
+      
+            </div>
+      
+          </section>
+          
+    </body>
+    </html>
